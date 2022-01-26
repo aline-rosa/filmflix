7740b5e134d67300784cd453bc6a5eae0ce41391
Aline Rosa <aline.rosa.dev@gmail.com>
2022-01-26 20:25:36 -0300

    Commit final

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { TvDetailComponent } from './tv-detail/tv-detail.component';
+
+const redirectUnauthorizedToLogin = () =>
+  redirectUnauthorizedTo(['/auth/login']);
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'perfil',
+    component: ProfileComponent,
+    ...canActivate(redirectUnauthorizedToLogin),
+  },
+  {
+    path: 'movie/:id',
+    component: MovieDetailComponent,
+  },
+  {
+    path: 'tv/:id',
+    component: TvDetailComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HomeRoutingModule {}
